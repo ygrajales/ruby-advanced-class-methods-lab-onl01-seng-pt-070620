8eeeb8826adcfc2191fd8744c8e8312d0dba852b
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-28 03:14:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,11 +13,18 @@ class Song
   def self.all
     @@all
   end
+  
+  def self.new_by_name(name)
+    song = self.new 
+    song.name = name
+    song.save
+    song
+  end
+    
 
   def save
     self.class.all << self
   end
-
 
 
 end
