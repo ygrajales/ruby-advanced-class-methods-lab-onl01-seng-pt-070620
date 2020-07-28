f9e6e9bbcd6986a4cc6cd832b75a3d7e8b430f75
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-07-28 03:34:22 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,9 +39,7 @@ class Song
   
   
   def self.find_or_create_by_name(name)
-    
-    self.create_by_name if self.find_by_name == false
-    
+    self.find_by_name(name) || self.create_by_name(name)
   end
       
   
